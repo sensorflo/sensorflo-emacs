575c5a0d04eaac19658fd4425c74b43288d62543
Florian Kaufmann <sensorflo@gmail.com>
2012-02-19 23:47:04 +0100

    added further *.el and submodules

---
+++ progmodes/rl-mode.el
@@ -0,0 +1,99 @@
+;;; rl-mode.el --- a major-mode for editing readline init files
+;; 
+;; Copyright 2009-2011 Florian Kaufmann <sensorflo@gmail.com>
+;;
+;; Filename: rl-mode.el
+;; Description: a major-mode for editing readline init files
+;; Author: Florian Kaufmann <sensorflo@gmail.com>
+;; Created: 2009
+;; 
+;; This file is not part of GNU Emacs.
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+
+;;; Variables
+
+(defconst rl-mode-version "0.1")
+
+;;; Code
+
+(defconst rl-font-lock-keywords
+  (eval-when-compile
+    (list
+     (list "^\\s-*\\(set\\)\\s-*\\(\\sw+\\)\\s-*\\(\\sw+\\)?" (list 1 'font-lock-keyword-face) (list 2 'font-lock-variable-name-face) (list 3 'font-lock-constant-face)) 
+     (cons "^#.*\n" 'font-lock-comment-face) 
+     (list "^\\(.*\\):" (list 1 'font-lock-constant-face)) 
+     (list "^\\(\\$\\sw+\\)" (list 1 'font-lock-preprocessor-face)) 
+     )))
+
+(defun rl-show-version ()
+  "Show the version number in the minibuffer."
+  (interactive)
+  (message "rl-mode, version %s" rl-mode-version))
+
+;;;###autoload
+(define-derived-mode rl-mode text-mode "rl"
+  "Major mode for editing readline init files.
+Turning on rl mode runs the normal hook `rl-mode-hook'."
+  (interactive)
+  
+  ;; syntax table
+  (modify-syntax-entry ?- "w")
+
+  ;; comments
+  (set (make-local-variable 'comment-column) 0)
+  (set (make-local-variable 'comment-start) "# ")
+  (set (make-local-variable 'comment-end) "")
+  (set (make-local-variable 'comment-start-skip) "^#\\s-*")
+  (set (make-local-variable 'comment-end-skip) "\\s-*\n")
+  
+  ;; TODO: make paragraphs
+  ;; paragraphs
+  ;; (set (make-local-variable 'paragraph-separate) "")
+  ;; (set (make-local-variable 'paragraph-start) "")
+  ;; (set (make-local-variable 'paragraph-ignore-fill-prefix) t)
+
+  ;; pages
+  ;; (set (make-local-variable 'page-delimiter) "^<<<+$") 
+  
+  ;; misc
+  (set (make-local-variable 'require-final-newline) t)
+
+  ;; font lock
+  (make-local-variable 'font-lock-defaults)
+  (setq font-lock-defaults 
+	'(rl-font-lock-keywords nil nil ((?_ . "w")) ))
+  
+  ;; outline mode
+  ;; BUG: if there are many spaces\tabs after =, level becomes wrong
+  ;; (set (make-local-variable 'outline-regexp) "^=\\{1,5\\}[ \t]+[^ \t\n]")
+
+  ;; TODO: font-lock-mark-block-function etc
+  ;; TODO: indent-functions
+  ;; TODO: outline
+  ;; TODO: spell check: do/dont check comments. only check 'real' text, i.e.
+  ;; text that is also in the output.
+  ;; spell: is it possible to attach an additional dictionary to certain files,
+  ;; i.e. to dragon projects words as pp dc mc would be added.
+  
+  ;; 
+  (run-hooks 'rl-mode-hook))
+
+(add-to-list 'auto-mode-alist '("\\.?inputrc\\'" . rl-mode))
+
+(provide 'rl-mode)
+
+;;; rl-mode.el ends here
---
+++ progmodes/stream-mode.el
@@ -0,0 +1,83 @@
+;;; stream-mode.el --- a major-mode for editing indel stream files in emacs
+;; 
+;; Copyright 2010-2011 Florian Kaufmann <sensorflo@gmail.com>
+;;
+;; Author: Florian Kaufmann <sensorflo@gmail.com>
+;; URL: http://sensorflo-emacs.googlecode.com/svn/trunk/stream-mode.el
+;; Created: 2010
+;; Keywords: languages
+;; 
+;; This file is not part of GNU Emacs.
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;; 
+;;; Variables
+
+(defconst stream-mode-version "0.1")
+
+(defvar stream-mode-hook nil
+  "Normal hook run when entering stream mode.")
+
+;;; Code
+
+(defconst stream-font-lock-keywords
+  (list
+   (cons "\\b\\(if\\|else\\|do\\|while\\|for\\|switch\\)\\b" font-lock-keyword-face)
+   (list "#{\\([^}\n]*\\)}" '(1 font-lock-variable-name-face))
+   (cons ":\\(\\w\\)" font-lock-keyword-face)
+   (cons "\\b\\([0-9]+\.[0-9]\\|\\.?[0-9]+\\|[0-9]+\\.?\\)\\b" font-lock-constant-face)
+   (list "\\b\\(Set\\) +\\(\\sw+\\) *=" '(1 font-lock-keyword-face) '(2 font-lock-variable-name-face))
+   ))
+
+;;;###autoload
+(define-derived-mode stream-mode text-mode "stream"
+  "Major mode for editing stream files.
+Turning on stream mode runs the normal hook `stream-mode-hook'."
+  (interactive)
+  
+  ;; syntax table
+  (modify-syntax-entry ?\" "\"")
+  (modify-syntax-entry ?\_ "w")
+  (modify-syntax-entry ?\n "> b")
+  (modify-syntax-entry ?\r "> b")
+  (modify-syntax-entry ?/  ". 124b")
+  (modify-syntax-entry ?*  ". 23")
+  
+  ;; comments
+  (set (make-local-variable 'comment-column) 0)
+  (set (make-local-variable 'comment-start) "// ")
+  (set (make-local-variable 'comment-end) "")
+  (set (make-local-variable 'comment-start-skip) "//\\s-*")
+  (set (make-local-variable 'comment-end-skip) "\\s-*\n")
+  
+  ;; TODO: make paragraphs using blank lines / OBJ_DEF 
+  ;; paragraphs
+  ;;   (set (make-local-variable 'paragraph-separate) xxx)
+  ;;   (set (make-local-variable 'paragraph-start) xxx )
+  ;;   (set (make-local-variable 'paragraph-ignore-fill-prefix) t)
+  
+  ;; misc
+  (set (make-local-variable 'require-final-newline) t)
+  
+  ;; font lock
+  (set (make-local-variable 'font-lock-defaults) '(stream-font-lock-keywords))
+  
+  ;; 
+  (run-hooks 'stream-mode-hook))
+
+(provide 'stream-mode)
+
+;;; stream-mode.el ends here

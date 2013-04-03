e239dce4f0bddfde21cced73b1df3077d0c7d6ad
Florian Kaufmann <sensorflo@gmail.com>
2013-04-03 15:52:19 +0200

    dragon: commit fix for adding coding system

--- projects/dragon.el
+++ projects/dragon.el
@@ -56,7 +56,7 @@
 
     (when (and (dragon-file-p) (not (dragon-coding-system-p)))
       (message (concat "%s: encoding system is %S which is not dragons's "
-                       "encoding system (iso-8859-1 windows-1252-dos "
+                       "encoding system (iso-latin-1-dos windows-1252-dos "
                        "undecided-dos utf-8-dos)")
                (buffer-name) buffer-file-coding-system)
       (shell-command (concat "notify-send -t 1000 "
@@ -339,7 +339,7 @@ sure what the good decisions are."
   ;;   ISO-8859-1 by using displayable characters rather than control characters
   ;;   in the 80 to 9F (hex) range...
   (member buffer-file-coding-system
-          '(iso-8859-1 windows-1252-dos undecided-dos utf-8-dos)))
+          '(iso-latin-1-dos windows-1252-dos undecided-dos utf-8-dos)))
 
 ;; note that this should somehow belong to the
 (defun dragon-file-p ()

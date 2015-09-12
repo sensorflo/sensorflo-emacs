8fc0015b9b6c1a3c1fa1df9fee516a6d51993196
sensorflo <sensorflo@gmail.com>
2015-09-12 22:12:16 +0200

    added some autoloads

--- customization/loaddefs-custom.el
+++ customization/loaddefs-custom.el
@@ -527,405 +527,6 @@ Visit the given directory in `sr-mode'.
 
 ;;;***
 
-;;;### (autoloads nil "../modified-site-lisp/whitespace" "../modified-site-lisp/whitespace.el"
-;;;;;;  (21143 32744 357499 181000))
-;;; Generated autoloads from ../modified-site-lisp/whitespace.el
-
-(autoload 'whitespace-mode "../modified-site-lisp/whitespace" "\
-Toggle whitespace minor mode visualization (\"ws\" on modeline).
-
-If ARG is null, toggle whitespace visualization.
-If ARG is a number greater than zero, turn on visualization;
-otherwise, turn off visualization.
-
-See also `whitespace-style', `whitespace-newline' and
-`whitespace-display-mappings'.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'whitespace-newline-mode "../modified-site-lisp/whitespace" "\
-Toggle NEWLINE minor mode visualization (\"nl\" on modeline).
-
-If ARG is null, toggle NEWLINE visualization.
-If ARG is a number greater than zero, turn on visualization;
-otherwise, turn off visualization.
-
-Use `whitespace-newline-mode' only for NEWLINE visualization
-exclusively.  For other visualizations, including NEWLINE
-visualization together with (HARD) SPACEs and/or TABs, please,
-use `whitespace-mode'.
-
-See also `whitespace-newline' and `whitespace-display-mappings'.
-
-\(fn &optional ARG)" t nil)
-
-(defvar global-whitespace-mode nil "\
-Non-nil if Global-Whitespace mode is enabled.
-See the command `global-whitespace-mode' for a description of this minor mode.
-Setting this variable directly does not take effect;
-either customize it (see the info node `Easy Customization')
-or call the function `global-whitespace-mode'.")
-
-(custom-autoload 'global-whitespace-mode "../modified-site-lisp/whitespace" nil)
-
-(autoload 'global-whitespace-mode "../modified-site-lisp/whitespace" "\
-Toggle whitespace global minor mode visualization (\"WS\" on modeline).
-
-If ARG is null, toggle whitespace visualization.
-If ARG is a number greater than zero, turn on visualization;
-otherwise, turn off visualization.
-
-See also `whitespace-style', `whitespace-newline' and
-`whitespace-display-mappings'.
-
-\(fn &optional ARG)" t nil)
-
-(defvar global-whitespace-newline-mode nil "\
-Non-nil if Global-Whitespace-Newline mode is enabled.
-See the command `global-whitespace-newline-mode' for a description of this minor mode.
-Setting this variable directly does not take effect;
-either customize it (see the info node `Easy Customization')
-or call the function `global-whitespace-newline-mode'.")
-
-(custom-autoload 'global-whitespace-newline-mode "../modified-site-lisp/whitespace" nil)
-
-(autoload 'global-whitespace-newline-mode "../modified-site-lisp/whitespace" "\
-Toggle NEWLINE global minor mode visualization (\"NL\" on modeline).
-
-If ARG is null, toggle NEWLINE visualization.
-If ARG is a number greater than zero, turn on visualization;
-otherwise, turn off visualization.
-
-Use `global-whitespace-newline-mode' only for NEWLINE
-visualization exclusively.  For other visualizations, including
-NEWLINE visualization together with (HARD) SPACEs and/or TABs,
-please, use `global-whitespace-mode'.
-
-See also `whitespace-newline' and `whitespace-display-mappings'.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'whitespace-toggle-options "../modified-site-lisp/whitespace" "\
-Toggle local `whitespace-mode' options.
-
-If local whitespace-mode is off, toggle the option given by ARG
-and turn on local whitespace-mode.
-
-If local whitespace-mode is on, toggle the option given by ARG
-and restart local whitespace-mode.
-
-Interactively, it reads one of the following chars:
-
-  CHAR	MEANING
-  (VIA FACES)
-   t	toggle TAB visualization
-   s	toggle SPACE and HARD SPACE visualization
-   r	toggle trailing blanks visualization
-   l	toggle \"long lines\" visualization
-   L	toggle \"long lines\" tail visualization
-   n	toggle NEWLINE visualization
-   e	toggle empty line at bob and/or eob visualization
-   C-i	toggle indentation SPACEs visualization (via `indent-tabs-mode')
-   I	toggle indentation SPACEs visualization
-   i	toggle indentation TABs visualization
-   C-a	toggle SPACEs after TAB visualization (via `indent-tabs-mode')
-   A	toggle SPACEs after TAB: SPACEs visualization
-   a	toggle SPACEs after TAB: TABs visualization
-   C-b	toggle SPACEs before TAB visualization (via `indent-tabs-mode')
-   B	toggle SPACEs before TAB: SPACEs visualization
-   b	toggle SPACEs before TAB: TABs visualization
-
-  (VIA DISPLAY TABLE)
-   T	toggle TAB visualization
-   S	toggle SPACEs before TAB visualization
-   N	toggle NEWLINE visualization
-
-   x	restore `whitespace-style' value
-   ?	display brief help
-
-Non-interactively, ARG should be a symbol or a list of symbols.
-The valid symbols are:
-
-   tabs			toggle TAB visualization
-   spaces		toggle SPACE and HARD SPACE visualization
-   trailing		toggle trailing blanks visualization
-   lines		toggle \"long lines\" visualization
-   lines-tail		toggle \"long lines\" tail visualization
-   newline		toggle NEWLINE visualization
-   empty		toggle empty line at bob and/or eob visualization
-   indentation		toggle indentation SPACEs visualization
-   indentation::tab	toggle indentation SPACEs visualization
-   indentation::space	toggle indentation TABs visualization
-   space-after-tab		toggle SPACEs after TAB visualization
-   space-after-tab::tab		toggle SPACEs after TAB: SPACEs visualization
-   space-after-tab::space	toggle SPACEs after TAB: TABs visualization
-   space-before-tab		toggle SPACEs before TAB visualization
-   space-before-tab::tab	toggle SPACEs before TAB: SPACEs visualization
-   space-before-tab::space	toggle SPACEs before TAB: TABs visualization
-
-   tab-mark		toggle TAB visualization
-   space-mark		toggle SPACEs before TAB visualization
-   newline-mark		toggle NEWLINE visualization
-
-   whitespace-style	restore `whitespace-style' value
-
-See `whitespace-style' and `indent-tabs-mode' for documentation.
-
-\(fn ARG)" t nil)
-
-(autoload 'global-whitespace-toggle-options "../modified-site-lisp/whitespace" "\
-Toggle global `whitespace-mode' options.
-
-If global whitespace-mode is off, toggle the option given by ARG
-and turn on global whitespace-mode.
-
-If global whitespace-mode is on, toggle the option given by ARG
-and restart global whitespace-mode.
-
-Interactively, it accepts one of the following chars:
-
-  CHAR	MEANING
-  (VIA FACES)
-   t	toggle TAB visualization
-   s	toggle SPACE and HARD SPACE visualization
-   r	toggle trailing blanks visualization
-   l	toggle \"long lines\" visualization
-   L	toggle \"long lines\" tail visualization
-   n	toggle NEWLINE visualization
-   e	toggle empty line at bob and/or eob visualization
-   C-i	toggle indentation SPACEs visualization (via `indent-tabs-mode')
-   I	toggle indentation SPACEs visualization
-   i	toggle indentation TABs visualization
-   C-a	toggle SPACEs after TAB visualization (via `indent-tabs-mode')
-   A	toggle SPACEs after TAB: SPACEs visualization
-   a	toggle SPACEs after TAB: TABs visualization
-   C-b	toggle SPACEs before TAB visualization (via `indent-tabs-mode')
-   B	toggle SPACEs before TAB: SPACEs visualization
-   b	toggle SPACEs before TAB: TABs visualization
-
-  (VIA DISPLAY TABLE)
-   T	toggle TAB visualization
-   S	toggle SPACEs before TAB visualization
-   N	toggle NEWLINE visualization
-
-   x	restore `whitespace-style' value
-   ?	display brief help
-
-Non-interactively, ARG should be a symbol or a list of symbols.
-The valid symbols are:
-
-   tabs			toggle TAB visualization
-   spaces		toggle SPACE and HARD SPACE visualization
-   trailing		toggle trailing blanks visualization
-   lines		toggle \"long lines\" visualization
-   lines-tail		toggle \"long lines\" tail visualization
-   newline		toggle NEWLINE visualization
-   empty		toggle empty line at bob and/or eob visualization
-   indentation		toggle indentation SPACEs visualization
-   indentation::tab	toggle indentation SPACEs visualization
-   indentation::space	toggle indentation TABs visualization
-   space-after-tab		toggle SPACEs after TAB visualization
-   space-after-tab::tab		toggle SPACEs after TAB: SPACEs visualization
-   space-after-tab::space	toggle SPACEs after TAB: TABs visualization
-   space-before-tab		toggle SPACEs before TAB visualization
-   space-before-tab::tab	toggle SPACEs before TAB: SPACEs visualization
-   space-before-tab::space	toggle SPACEs before TAB: TABs visualization
-
-   tab-mark		toggle TAB visualization
-   space-mark		toggle SPACEs before TAB visualization
-   newline-mark		toggle NEWLINE visualization
-
-   whitespace-style	restore `whitespace-style' value
-
-See `whitespace-style' and `indent-tabs-mode' for documentation.
-
-\(fn ARG)" t nil)
-
-(autoload 'whitespace-cleanup "../modified-site-lisp/whitespace" "\
-Cleanup some blank problems in all buffer or at region.
-
-It usually applies to the whole buffer, but in transient mark
-mode when the mark is active, it applies to the region.  It also
-applies to the region when it is not in transient mark mode, the
-mark is active and \\[universal-argument] was pressed just before
-calling `whitespace-cleanup' interactively.
-
-See also `whitespace-cleanup-region'.
-
-The problems cleaned up are:
-
-1. empty lines at beginning of buffer.
-2. empty lines at end of buffer.
-   If `whitespace-style' includes the value `empty', remove all
-   empty lines at beginning and/or end of buffer.
-
-3. 8 or more SPACEs at beginning of line.
-   If `whitespace-style' includes the value `indentation':
-   replace 8 or more SPACEs at beginning of line by TABs, if
-   `indent-tabs-mode' is non-nil; otherwise, replace TABs by
-   SPACEs.
-   If `whitespace-style' includes the value `indentation::tab',
-   replace 8 or more SPACEs at beginning of line by TABs.
-   If `whitespace-style' includes the value `indentation::space',
-   replace TABs by SPACEs.
-
-4. SPACEs before TAB.
-   If `whitespace-style' includes the value `space-before-tab':
-   replace SPACEs by TABs, if `indent-tabs-mode' is non-nil;
-   otherwise, replace TABs by SPACEs.
-   If `whitespace-style' includes the value
-   `space-before-tab::tab', replace SPACEs by TABs.
-   If `whitespace-style' includes the value
-   `space-before-tab::space', replace TABs by SPACEs.
-
-5. SPACEs or TABs at end of line.
-   If `whitespace-style' includes the value `trailing', remove
-   all SPACEs or TABs at end of line.
-
-6. 8 or more SPACEs after TAB.
-   If `whitespace-style' includes the value `space-after-tab':
-   replace SPACEs by TABs, if `indent-tabs-mode' is non-nil;
-   otherwise, replace TABs by SPACEs.
-   If `whitespace-style' includes the value
-   `space-after-tab::tab', replace SPACEs by TABs.
-   If `whitespace-style' includes the value
-   `space-after-tab::space', replace TABs by SPACEs.
-
-See `whitespace-style', `indent-tabs-mode' and `tab-width' for
-documentation.
-
-\(fn)" t nil)
-
-(autoload 'whitespace-cleanup-region "../modified-site-lisp/whitespace" "\
-Cleanup some blank problems at region.
-
-The problems cleaned up are:
-
-1. 8 or more SPACEs at beginning of line.
-   If `whitespace-style' includes the value `indentation':
-   replace 8 or more SPACEs at beginning of line by TABs, if
-   `indent-tabs-mode' is non-nil; otherwise, replace TABs by
-   SPACEs.
-   If `whitespace-style' includes the value `indentation::tab',
-   replace 8 or more SPACEs at beginning of line by TABs.
-   If `whitespace-style' includes the value `indentation::space',
-   replace TABs by SPACEs.
-
-2. SPACEs before TAB.
-   If `whitespace-style' includes the value `space-before-tab':
-   replace SPACEs by TABs, if `indent-tabs-mode' is non-nil;
-   otherwise, replace TABs by SPACEs.
-   If `whitespace-style' includes the value
-   `space-before-tab::tab', replace SPACEs by TABs.
-   If `whitespace-style' includes the value
-   `space-before-tab::space', replace TABs by SPACEs.
-
-3. SPACEs or TABs at end of line.
-   If `whitespace-style' includes the value `trailing', remove
-   all SPACEs or TABs at end of line.
-
-4. 8 or more SPACEs after TAB.
-   If `whitespace-style' includes the value `space-after-tab':
-   replace SPACEs by TABs, if `indent-tabs-mode' is non-nil;
-   otherwise, replace TABs by SPACEs.
-   If `whitespace-style' includes the value
-   `space-after-tab::tab', replace SPACEs by TABs.
-   If `whitespace-style' includes the value
-   `space-after-tab::space', replace TABs by SPACEs.
-
-See `whitespace-style', `indent-tabs-mode' and `tab-width' for
-documentation.
-
-\(fn START END)" t nil)
-
-(autoload 'whitespace-report "../modified-site-lisp/whitespace" "\
-Report some whitespace problems in buffer.
-
-Return nil if there is no whitespace problem; otherwise, return
-non-nil.
-
-If FORCE is non-nil or \\[universal-argument] was pressed just
-before calling `whitespace-report' interactively, it forces
-`whitespace-style' to have:
-
-   empty
-   trailing
-   indentation
-   space-before-tab
-   space-after-tab
-
-If REPORT-IF-BOGUS is non-nil, it reports only when there are any
-whitespace problems in buffer.
-
-Report if some of the following whitespace problems exist:
-
-* If `indent-tabs-mode' is non-nil:
-   empty		1. empty lines at beginning of buffer.
-   empty		2. empty lines at end of buffer.
-   trailing		3. SPACEs or TABs at end of line.
-   indentation		4. 8 or more SPACEs at beginning of line.
-   space-before-tab	5. SPACEs before TAB.
-   space-after-tab	6. 8 or more SPACEs after TAB.
-
-* If `indent-tabs-mode' is nil:
-   empty		1. empty lines at beginning of buffer.
-   empty		2. empty lines at end of buffer.
-   trailing		3. SPACEs or TABs at end of line.
-   indentation		4. TABS at beginning of line.
-   space-before-tab	5. SPACEs before TAB.
-   space-after-tab	6. 8 or more SPACEs after TAB.
-
-See `whitespace-style' for documentation.
-See also `whitespace-cleanup' and `whitespace-cleanup-region' for
-cleaning up these problems.
-
-\(fn &optional FORCE REPORT-IF-BOGUS)" t nil)
-
-(autoload 'whitespace-report-region "../modified-site-lisp/whitespace" "\
-Report some whitespace problems in a region.
-
-Return nil if there is no whitespace problem; otherwise, return
-non-nil.
-
-If FORCE is non-nil or \\[universal-argument] was pressed just
-before calling `whitespace-report-region' interactively, it
-forces `whitespace-style' to have:
-
-   empty
-   indentation
-   space-before-tab
-   trailing
-   space-after-tab
-
-If REPORT-IF-BOGUS is non-nil, it reports only when there are any
-whitespace problems in buffer.
-
-Report if some of the following whitespace problems exist:
-
-* If `indent-tabs-mode' is non-nil:
-   empty		1. empty lines at beginning of buffer.
-   empty		2. empty lines at end of buffer.
-   trailing		3. SPACEs or TABs at end of line.
-   indentation		4. 8 or more SPACEs at beginning of line.
-   space-before-tab	5. SPACEs before TAB.
-   space-after-tab	6. 8 or more SPACEs after TAB.
-
-* If `indent-tabs-mode' is nil:
-   empty		1. empty lines at beginning of buffer.
-   empty		2. empty lines at end of buffer.
-   trailing		3. SPACEs or TABs at end of line.
-   indentation		4. TABS at beginning of line.
-   space-before-tab	5. SPACEs before TAB.
-   space-after-tab	6. 8 or more SPACEs after TAB.
-
-See `whitespace-style' for documentation.
-See also `whitespace-cleanup' and `whitespace-cleanup-region' for
-cleaning up these problems.
-
-\(fn START END &optional FORCE REPORT-IF-BOGUS)" t nil)
-
-;;;***
-
 ;;;### (autoloads nil "../modified-site-lisp/x-dict" "../modified-site-lisp/x-dict.el"
 ;;;;;;  (21741 63343 798084 765000))
 ;;; Generated autoloads from ../modified-site-lisp/x-dict.el
@@ -1089,6 +690,48 @@ Major mode for editing Zimbu code.
 Turning on zimbu mode runs the normal hook `zimbu-mode-hook'.
 
 \(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "../site-lisp/fill-column-indicator" "../site-lisp/fill-column-indicator.el"
+;;;;;;  (21143 32520 849501 109000))
+;;; Generated autoloads from ../site-lisp/fill-column-indicator.el
+
+(autoload 'fci-mode "../site-lisp/fill-column-indicator" "\
+Toggle fci-mode on and off.
+Fci-mode indicates the location of the fill column by drawing a
+thin line (a `rule') at the fill column.
+
+With prefix ARG, turn fci-mode on if and only if ARG is positive.
+
+The following options control the appearance of the fill-column
+rule: `fci-rule-column', `fci-rule-width', `fci-rule-color',
+`fci-rule-use-dashes', `fci-dash-pattern', `fci-rule-character',
+and `fci-rule-character-color'.  For further options, see the
+Customization menu or the package file.  (See the latter for tips
+on troubleshooting.)
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'turn-on-fci-mode "../site-lisp/fill-column-indicator" "\
+Turn on fci-mode unconditionally.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "../site-lisp/llvm-mode" "../site-lisp/llvm-mode.el"
+;;;;;;  (21782 62671 405803 88000))
+;;; Generated autoloads from ../site-lisp/llvm-mode.el
+
+(autoload 'llvm-mode "../site-lisp/llvm-mode" "\
+Major mode for editing LLVM source files.
+\\{llvm-mode-map}
+  Runs `llvm-mode-hook' on startup.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist (cons (purecopy "\\.ll\\'") 'llvm-mode))
 
 ;;;***
 

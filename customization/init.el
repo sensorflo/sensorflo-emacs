a0764005d18c1466717fe7bb6f6dca1236199276
sensorflo <sensorflo@gmail.com>
2015-11-24 09:04:25 +0100

    init: added debian package debian-el

--- customization/init.el
+++ customization/init.el
@@ -63,6 +63,7 @@ Meant to profile startup time."
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/dvc/")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp")
 (add-to-list 'load-path "/usr/share/emacs24/site-lisp/emacs-goodies-el")
+(add-to-list 'load-path "/usr/share/emacs24/site-lisp/debian-el")
 
 ;; find-function-C-source-directory
 (setq find-function-C-source-directory
@@ -87,6 +88,7 @@ Meant to profile startup time."
 (auto-compile-on-save-mode 1)
 
 ;; autoload
+(load-library "debian-el-loaddefs")
 (load-library "emacs-goodies-loaddefs")
 ;; intendet for own libraries
 (message2 "autoload loaddefs-custom")
